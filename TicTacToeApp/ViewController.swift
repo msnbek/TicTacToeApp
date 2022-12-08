11e027759939037a68ed61d6fbbc6b4515d87897
msnbek <mahmutsenbek@icloud.com>
2022-12-08 16:17:47 +0300

    addToBoard func added.

--- TicTacToeApp/ViewController.swift
+++ TicTacToeApp/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    enum Turn {
+        case Circular
+        case Cross
+    }
+    
+    @IBOutlet weak var turnLabel: UILabel!
+    @IBOutlet weak var a1Btn: UIButton!
+    @IBOutlet weak var a2Btn: UIButton!
+    @IBOutlet weak var a3Btn: UIButton!
+    @IBOutlet weak var b1Btn: UIButton!
+    @IBOutlet weak var b2Btn: UIButton!
+    @IBOutlet weak var b3Btn: UIButton!
+    @IBOutlet weak var c1Btn: UIButton!
+    @IBOutlet weak var c2Btn: UIButton!
+    @IBOutlet weak var c3Btn: UIButton!
+    
+    var firstTurn = Turn.Cross
+    var currentTurn = Turn.Cross
+    
+    var CIRCULAR = "O"
+    var CROSS = "X"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        
+        addToBoard(sender)
+    }
+    
+    
+    func addToBoard(_ sender: UIButton) {
+        
+        if sender.title(for: .normal) == nil {
+            if currentTurn == Turn.Circular {
+                sender.setTitle(CIRCULAR, for: UIControl.State.normal)
+                currentTurn = Turn.Cross
+                turnLabel.text = CROSS
+            }else if  currentTurn == Turn.Cross {
+                sender.setTitle(CROSS, for: UIControl.State.normal)
+                currentTurn = Turn.Circular
+                turnLabel.text = CIRCULAR
+            }
+            
+            sender.isEnabled = false
+        }
+        
+    }
+    
 
 }
 
